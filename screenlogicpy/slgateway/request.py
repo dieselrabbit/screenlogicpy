4f74262273fa562368db251700b2a658f8bcb185
dieselrabbit <37058192+dieselrabbit@users.noreply.github.com>
2020-02-11 00:05:15 -0800

    Initial restructure
    Continued local imports.
    Refine * imports

--- screenlogicpy/slgateway/request.py
+++ screenlogicpy/slgateway/request.py
@@ -1,8 +1,10 @@
+import sys
 import socket
-from slgateway.msgutil import *
-from slgateway.const import code
-from slgateway.decode_response.config import decode_pool_config_response
-from slgateway.decode_response.status import decode_pool_status_response
+import struct
+from . msgutil import getMessageString, makeMessage, takeMessage
+from . const import me, code
+from . decode_response.config import decode_pool_config_response
+from . decode_response.status import decode_pool_status_response
 
 def request_gateway(gateway_socket):
     gateway_socket.sendall(makeMessage(code.VERSION_QUERY))
@@ -11,7 +13,7 @@ def request_gateway(gateway_socket):
         sys.stderr.write("WARNING: {}: no {} data received.\n".format(me, "VERSION_ANSWER"))
     rcvcode, buff = takeMessage(data)
     if (rcvcode != code.VERSION_ANSWER):
-        sys.stderr.write("WARNING: {}: rcvCode({}) != {}.\n".format(me, rcvCode2, code.VERSION_ANSWER))
+        sys.stderr.write("WARNING: {}: rcvCode({}) != {}.\n".format(me, rcvcode, code.VERSION_ANSWER))
         return False
         #sys.exit(10)
     return getMessageString(buff)
@@ -29,7 +31,7 @@ def request_pool_status(gateway_socket, data):
     gateway_socket.sendall(makeMessage(code.POOLSTATUS_QUERY, struct.pack("<I", 0)))
     rcvcode, buff = takeMessage(gateway_socket.recv(1024))
     if (rcvcode != code.POOLSTATUS_ANSWER):
-        sys.stderr.write("WARNING: {}: rcvCode({}) != {}.\n".format(me, rcvCode, code.POOLSTATUS_ANSWER))
+        sys.stderr.write("WARNING: {}: rcvCode({}) != {}.\n".format(me, rcvcode, code.POOLSTATUS_ANSWER))
         return False
         #sys.exit(11)
     decode_pool_status_response(buff, data)
@@ -38,7 +40,7 @@ def request_pool_button_press(gateway_socket, circuit_id, circuit_state):
     gateway_socket.sendall(makeMessage(code.BUTTONPRESS_QUERY, struct.pack("<III", 0, circuit_id, circuit_state)))
     rcvcode, buff = takeMessage(gateway_socket.recv(1024))
     if (rcvcode != code.BUTTONPRESS_ANSWER):
-        sys.stderr.write("WARNING: {}: rcvCode({}) != {}.\n".format(me, rcvCode, code.BUTTONPRESS_ANSWER))
+        sys.stderr.write("WARNING: {}: rcvCode({}) != {}.\n".format(me, rcvcode, code.BUTTONPRESS_ANSWER))
         return False
         #sys.exit(11)
     #print(rcvcode)
@@ -48,7 +50,7 @@ def request_set_heat_setpoint(gateway_socket, body, setpoint):
     gateway_socket.sendall(makeMessage(code.SETHEATTEMP_QUERY, struct.pack("<III", 0, body, setpoint)))
     rcvcode, buff = takeMessage(gateway_socket.recv(1024))
     if (rcvcode != code.SETHEATTEMP_ANSWER):
-        sys.stderr.write("WARNING: {}: rcvCode({}) != {}.\n".format(me, rcvCode, code.BUTTONPRESS_ANSWER))
+        sys.stderr.write("WARNING: {}: rcvCode({}) != {}.\n".format(me, rcvcode, code.BUTTONPRESS_ANSWER))
         return False
         #sys.exit(11)
     #print(rcvcode)
@@ -58,7 +60,7 @@ def request_set_heat_mode(gateway_socket, body, heat_mode):
     gateway_socket.sendall(makeMessage(code.SETHEATMODE_QUERY, struct.pack("<III", 0, body, heat_mode)))
     rcvcode, buff = takeMessage(gateway_socket.recv(1024))
     if (rcvcode != code.SETHEATMODE_ANSWER):
-        sys.stderr.write("WARNING: {}: rcvCode({}) != {}.\n".format(me, rcvCode, code.BUTTONPRESS_ANSWER))
+        sys.stderr.write("WARNING: {}: rcvCode({}) != {}.\n".format(me, rcvcode, code.BUTTONPRESS_ANSWER))
         return False
         #sys.exit(11)
     #print(rcvcode)
