fb81dd63b94647d29db0bd31aa16291c6085860d
Kevin Worrel <kevinworrel@yahoo.com>
2021-02-03 23:43:55 -0800

    Update test_cli to run on linux

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,13 @@
 import os
+import platform
 
-return_code = os.system("C:/Users/Kevin/AppData/Local/Programs/Python/Python36-32/python.exe -m screenlogicpy.cli get json")
+command = "screenlogicpy.cli get json"
+
+python = ""
+if platform.system() == "Windows":
+    python = "C:/Users/Kevin/AppData/Local/Programs/Python/Python36-32/python.exe"
+else:
+    python = "/usr/bin/python3.6"
+
+return_code = os.system("{} -m {}".format(python, command))
 #print(return_code)
