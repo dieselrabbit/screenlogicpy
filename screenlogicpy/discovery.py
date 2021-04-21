5281051180cadeb2c1b977ae9ea8b5263f50b969
J. Nick Koston <nick@koston.org>
2021-04-20 15:17:47 -1000

    Implement async_discover

--- screenlogicpy/discovery.py
+++ screenlogicpy/discovery.py
@@ -1,33 +1,120 @@
+"""Discovery for screenlogic gateways."""
+import asyncio
+import ipaddress
+import logging
 import socket
 import struct
-import ipaddress
-from .const import (
-    ScreenLogicError,
+
+from .const import (  # pylint: disable=relative-beyond-top-level
     SL_GATEWAY_IP,
-    SL_GATEWAY_PORT,
-    SL_GATEWAY_TYPE,
-    SL_GATEWAY_SUBTYPE,
     SL_GATEWAY_NAME,
+    SL_GATEWAY_PORT,
+    SL_GATEWAY_SUBTYPE,
+    SL_GATEWAY_TYPE,
+    ScreenLogicError,
 )
+
+DISCOVERY_PAYLOAD = struct.pack("<bbbbbbbb", 1, 0, 0, 0, 0, 0, 0, 0)
+DISCOVERY_RESPONSE_FORMAT = "<I4BH2B"
+DISCOVERY_RESPONSE_SIZE = struct.calcsize(DISCOVERY_RESPONSE_FORMAT)
+DISCOVERY_ADDRESS = "255.255.255.255"
+DISCOVERY_PORT = 1444
+DISCOVERY_CHKSUM = 2
+DISCOVERY_TIMEOUT = 1
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def create_broadcast_socket():
+    """Create a broadcast soscket for discovery."""
+    addressfamily = socket.AF_INET
+    udp_sock = socket.socket(addressfamily, socket.SOCK_DGRAM)
+    udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    return udp_sock
+
+
+def process_discovery_response(data):
+    """Process a discovery response."""
+    paddedfmt = (
+        DISCOVERY_RESPONSE_FORMAT + str(len(data) - DISCOVERY_RESPONSE_SIZE) + "s"
+    )
+    (
+        chk,
+        ip1,
+        ip2,
+        ip3,
+        ip4,
+        gateway_port,
+        gateway_type,
+        gateway_subtype,
+        gateway_name,
+    ) = struct.unpack(paddedfmt, data)
+
+    # not sure we need to exit if "chk" isn't what we wanted.
+    if chk != DISCOVERY_CHKSUM:
+        raise ScreenLogicError("ScreenLogic Discovery: Unexpected response checksum.")
+
+    # make sure we got a good IP address
+    received_ip = f"{str(ip1)}.{str(ip2)}.{str(ip3)}.{str(ip4)}"
+    try:
+        gateway_ip = str(ipaddress.ip_address(received_ip))
+    except ValueError as err:
+        raise ScreenLogicError(
+            "ScreenLogic Discovery: Got an invalid IP address from the gateway."
+        ) from err
+    except NameError as err:
+        raise ScreenLogicError(
+            "ScreenLogic Discovery: Received garbage from the gateway."
+        ) from err
+    except Exception as err:
+        raise ScreenLogicError(
+            "ScreenLogic Discovery: Couldn't get an IP address for the gateway."
+        ) from err
+
+    return {
+        SL_GATEWAY_IP: gateway_ip,
+        SL_GATEWAY_PORT: gateway_port,
+        SL_GATEWAY_TYPE: gateway_type,
+        SL_GATEWAY_SUBTYPE: gateway_subtype,
+        SL_GATEWAY_NAME: gateway_name.decode("utf-8").strip("\0"),
+    }
+
+
+class ScreenLogicDiscoveryProtocol:
+    """Implement ScreenLogic discovery protocol."""
+
+    def __init__(self):
+        """Init protocol."""
+        self.transport = None
+        self.hosts = []
+
+    def connection_lost(self, _):
+        """Connection lost."""
+
+    def connection_made(self, transport):
+        """Connection made."""
+        self.transport = transport
+
+    def datagram_received(self, data, _):
+        """Response recieved."""
+        try:
+            self.hosts.append(process_discovery_response(data))
+        except Exception as ex:  # pylint: disable=broad-except
+            _LOGGER.warning(ex)
+
+    def error_received(self, exc):
+        """Error received."""
 
 
 def discover():
-    step = "ScreenLogic Discovery"
-    broadcast = "255.255.255.255"
-    port = 1444
-    addressfamily = socket.AF_INET
-    wantchk = 2
-
-    data = struct.pack("<bbbbbbbb", 1, 0, 0, 0, 0, 0, 0, 0)
-    udpSock = socket.socket(addressfamily, socket.SOCK_DGRAM)
-    udpSock.settimeout(1)
-    udpSock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    """Discover screenlogic gateways."""
+    udp_sock = create_broadcast_socket()
+    udp_sock.settimeout(DISCOVERY_TIMEOUT)
     responses = []
     try:
-        udpSock.sendto(data, (broadcast, port))
-
+        udp_sock.sendto(DISCOVERY_PAYLOAD, (DISCOVERY_ADDRESS, DISCOVERY_PORT))
         while True:
-            response = udpSock.recvfrom(4096)
+            response = udp_sock.recvfrom(4096)
             if response:
                 responses.append(response)
             else:
@@ -35,52 +122,22 @@ def discover():
     except socket.timeout:
         pass
     finally:
-        udpSock.close()
+        udp_sock.close()
 
-    expectedfmt = "<I4BH2B"
-    hosts = []
-    for data, _ in responses:
-        paddedfmt = expectedfmt + str(len(data) - struct.calcsize(expectedfmt)) + "s"
-        (
-            chk,
-            ip1,
-            ip2,
-            ip3,
-            ip4,
-            gatewayPort,
-            gatewayType,
-            gatewaySubtype,
-            gatewayName,
-        ) = struct.unpack(paddedfmt, data)
+    return [process_discovery_response(response[0]) for response in responses]
 
-        # not sure we need to exit if "chk" isn't what we wanted.
-        if chk != wantchk:
-            raise ScreenLogicError(f"{step}: Unexpected response checksum.")
 
-        # make sure we got a good IP address
-        receivedIP = f"{str(ip1)}.{str(ip2)}.{str(ip3)}.{str(ip4)}"
-        try:
-            gatewayIP = str(ipaddress.ip_address(receivedIP))
-        except ValueError as err:
-            raise ScreenLogicError(
-                f"{step}: Got an invalid IP address from the gateway."
-            ) from err
-        except NameError as err:
-            raise ScreenLogicError(
-                f"{step}: Received garbage from the gateway."
-            ) from err
-        except Exception as err:
-            raise ScreenLogicError(
-                f"{step}: Couldn't get an IP address for the gateway."
-            ) from err
-
-        host = {
-            SL_GATEWAY_IP: gatewayIP,
-            SL_GATEWAY_PORT: gatewayPort,
-            SL_GATEWAY_TYPE: gatewayType,
-            SL_GATEWAY_SUBTYPE: gatewaySubtype,
-            SL_GATEWAY_NAME: gatewayName.decode("utf-8").strip("\0"),
-        }
-        hosts.append(host)
-
-    return hosts
+async def async_discover():
+    """Discover screenlogic gateways."""
+    loop = asyncio.get_event_loop()
+    udp_sock = create_broadcast_socket()
+    transport, protocol = await loop.create_datagram_endpoint(
+        lambda: ScreenLogicDiscoveryProtocol(),  # pylint: disable=unnecessary-lambda
+        sock=udp_sock,
+    )
+    try:
+        transport.sendto(DISCOVERY_PAYLOAD, (DISCOVERY_ADDRESS, DISCOVERY_PORT))
+        await asyncio.sleep(DISCOVERY_TIMEOUT)
+    finally:
+        transport.close()
+    return protocol.hosts
