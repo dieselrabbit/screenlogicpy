d823cec7bcc2947feb1b6c03ede3f04b316a7e01
Kevin Worrel <37058192+dieselrabbit@users.noreply.github.com>
2021-04-21 23:41:49 -0700

    Min python to 3.8, version to 0.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="screenlogicpy",
-    version="0.3.1",
+    version="0.4.0",
     author="Kevin Worrel",
     author_email="kevinworrel@yahoo.com",
     description="Interface for Pentair ScreenLogic connected pool controllers over IP via Python",
@@ -20,6 +20,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
         "Development Status :: 3 - Alpha",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     entry_points={"console_scripts": ["screenlogicpy=screenlogicpy.cli:cli"]},
 )
