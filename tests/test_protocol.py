4739016c098d9b663c16e6fac26f243d394587e3
Kevin Worrel <37058192+dieselrabbit@users.noreply.github.com>
2021-10-22 00:22:31 -0700

    Update

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -1,23 +1,24 @@
-import asyncio
+import pytest
 
 from screenlogicpy.requests.protocol import ScreenLogicProtocol
 from screenlogicpy.requests.utility import makeMessage
 
 
-def test_async_data_received():
+@pytest.mark.asyncio
+async def test_async_data_received(event_loop):
     CODE = 1196
     SENDER = 0
-    MESSAGE = b""
+    MESSAGE = b"success"
 
     data = {}
 
     def callback(messageCode, senderID, message, target_data):
-        target_data["success"] = True
+        target_data["result"] = message
 
-    protocol = ScreenLogicProtocol(asyncio.get_event_loop())
+    protocol = ScreenLogicProtocol(event_loop)
     protocol.register_async_message_callback(CODE, callback, data)
 
     payload = makeMessage(CODE, MESSAGE, SENDER)
     protocol.data_received(payload)
 
-    assert data.get("success") is True
+    assert data.get("result") == MESSAGE
