3e53234c1e9aa972615eefd8abdf45bf1160fadb
Kevin Worrel <37058192+dieselrabbit@users.noreply.github.com>
2021-10-23 01:04:12 -0700

    Update python versions

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ setuptools.setup(
     url="https://github.com/dieselrabbit/screenlogicpy",
     packages=setuptools.find_packages(include=["screenlogicpy", "screenlogicpy.*"]),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Development Status :: 3 - Alpha",
