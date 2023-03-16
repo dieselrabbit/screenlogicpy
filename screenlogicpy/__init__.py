fa45b129f935cfeebdb9b738ae6c179c7e701a92
Kevin Worrel <37058192+dieselrabbit@users.noreply.github.com>
2023-03-16 07:56:21 -0700

    Bump version to v0.8.2

--- screenlogicpy/__init__.py
+++ screenlogicpy/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.8.1"
+__version__ = "0.8.2"
 # flake8: noqa F401
 from screenlogicpy.gateway import ScreenLogicGateway
 from screenlogicpy.const import ScreenLogicError, ScreenLogicRequestError
