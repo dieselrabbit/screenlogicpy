06f36a3bddf3cd5e09d43f5c6cca9004a179ead5
Kevin Worrel <37058192+dieselrabbit@users.noreply.github.com>
2023-09-03 16:29:04 -0700

    Spec v0.10.0, Target python 3.10

--- screenlogicpy/__init__.py
+++ screenlogicpy/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.9.1"
+__version__ = "0.10.0"
 # flake8: noqa F401
 from screenlogicpy.gateway import ScreenLogicGateway
 from screenlogicpy.const.common import ScreenLogicError, ScreenLogicRequestError
