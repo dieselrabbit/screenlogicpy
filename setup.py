e1179df43cb4650c50514c2e6aba019e52ee68a4
Kevin Worrel <37058192+dieselrabbit@users.noreply.github.com>
2023-09-03 16:29:04 -0700

    Spec v0.10.0, Target python 3.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="screenlogicpy",
-    version="0.9.1",
+    version="0.10.0",
     author="Kevin Worrel",
     author_email="kevinworrel@yahoo.com",
     description="Interface for Pentair ScreenLogic connected pool controllers over IP via Python",
@@ -15,14 +15,13 @@ setuptools.setup(
     url="https://github.com/dieselrabbit/screenlogicpy",
     packages=setuptools.find_packages(include=["screenlogicpy", "screenlogicpy.*"]),
     classifiers=[
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Development Status :: 3 - Alpha",
     ],
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     install_requires=[
         "async_timeout>=3.0.0",
     ],
