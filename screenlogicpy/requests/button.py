aebcebd9d9063cd44f9d3596a64eec5e04b15e90
Kevin Worrel <37058192+dieselrabbit@users.noreply.github.com>
2023-10-29 15:11:16 -0700

    Raise exception on unexpected response

--- screenlogicpy/requests/button.py
+++ screenlogicpy/requests/button.py
@@ -1,5 +1,6 @@
 import struct
 
+from ..const.common import ScreenLogicResponseError
 from ..const.msg import CODE
 from .protocol import ScreenLogicProtocol
 from .request import async_make_request
@@ -8,12 +9,17 @@ from .request import async_make_request
 async def async_request_pool_button_press(
     protocol: ScreenLogicProtocol, circuit_id: int, circuit_state: int, max_retries: int
 ) -> bool:
-    return (
-        await async_make_request(
-            protocol,
-            CODE.BUTTONPRESS_QUERY,
-            struct.pack("<III", 0, circuit_id, circuit_state),
-            max_retries,
+    if not (
+        (
+            response := await async_make_request(
+                protocol,
+                CODE.BUTTONPRESS_QUERY,
+                struct.pack("<III", 0, circuit_id, circuit_state),
+                max_retries,
+            )
         )
         == b""
-    )
+    ):
+        raise ScreenLogicResponseError(
+            f"Set circuit failed. Unexpected response: {response}"
+        )
--- screenlogicpy/requests/heat.py
+++ screenlogicpy/requests/heat.py
@@ -1,5 +1,6 @@
 import struct
 
+from ..const.common import ScreenLogicResponseError
 from ..const.msg import CODE
 from .protocol import ScreenLogicProtocol
 from .request import async_make_request
@@ -8,26 +9,30 @@ from .request import async_make_request
 async def async_request_set_heat_setpoint(
     protocol: ScreenLogicProtocol, body: int, set_point: float, max_retries: int
 ) -> bool:
-    return (
-        await async_make_request(
+    if (
+        response := await async_make_request(
             protocol,
             CODE.SETHEATTEMP_QUERY,
             struct.pack("<III", 0, body, set_point),
             max_retries,
         )
-        == b""
-    )
+    ) != b"":
+        raise ScreenLogicResponseError(
+            f"Set heat setpoint failed. Unexpected response: {response}"
+        )
 
 
 async def async_request_set_heat_mode(
     protocol: ScreenLogicProtocol, body: int, heat_mode: int, max_retries: int
 ) -> bool:
-    return (
-        await async_make_request(
+    if (
+        response := await async_make_request(
             protocol,
             CODE.SETHEATMODE_QUERY,
             struct.pack("<III", 0, body, heat_mode),
             max_retries,
         )
-        == b""
-    )
+    ) != b"":
+        raise ScreenLogicResponseError(
+            f"Set heat mode failed. Unexpected response: {response}"
+        )
