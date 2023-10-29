5baee43ae618256df56c53a33be46ae3156d65ba
Kevin Worrel <37058192+dieselrabbit@users.noreply.github.com>
2023-10-29 15:09:55 -0700

    New exception classes

--- screenlogicpy/const/common.py
+++ screenlogicpy/const/common.py
@@ -34,6 +34,18 @@ class ScreenLogicRequestError(ScreenLogicException):
     pass
 
 
+class ScreenLogicConnectionError(ScreenLogicException):
+    pass
+
+
+class ScreenLogicResponseError(ScreenLogicException):
+    pass
+
+
+class ScreenLogicLoginError(ScreenLogicException):
+    pass
+
+
 class SLIntEnum(IntEnum):
     @classmethod
     def parse(cls, value: str, default=0) -> "SLIntEnum":
